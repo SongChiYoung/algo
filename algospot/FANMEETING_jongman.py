823d43de3d803593487027e7242e4196f85c2541
EeS <ees@ees.guru>
2022-01-14 14:20:50 +0900

    FIX(FANMEETING_jong): FIX a word case, but still wong answer

--- algospot/FANMEETING_jongman.py
+++ algospot/FANMEETING_jongman.py
@@ -6,7 +6,7 @@ C = int(input())
 for c in range(C):
     member = input().replace("F","0").replace("M", "1")
     fan = input().replace("F","0").replace("M", "1")
-    fan = fan[::-1]
+    member = member[::-1]
 
     m_l = len(member)
     f_l = len(fan)
@@ -14,9 +14,9 @@ for c in range(C):
     
     res = int(member)*int(fan)
 
-    f_head = "{0:0"+str(r_l)+"}"
-    res = f_head.format(res)
-    res = res[-f_l:-m_l+1]
-
+    f_head = "{0:0"+str(f_l)+"}"
+    res = f_head.format(res)[-f_l:]
+    if(1-m_l != 0):
+        res = res[:1-m_l]
+    
     print(res.count("0"))
-
